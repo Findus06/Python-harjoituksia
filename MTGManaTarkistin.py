5dca8c6f569c57477f52e7d25afc65d95ec4af68
Vili Rask <118250376+Findus06@users.noreply.github.com>
2023-05-29 15:29:21 +0300

    Add files via upload

--- MTGManaTarkistin.py
+++ MTGManaTarkistin.py
@@ -4,37 +4,56 @@ def tarkista_mana(manapooli, kortin_mana):
 
     for valu, maara in kortin_mana_sanakirja.items():
         if valu not in manapooli_sanakirja or manapooli_sanakirja[valu] < maara:
-            return False
+            if "neutraali" not in manapooli_sanakirja or manapooli_sanakirja["neutraali"] < (maara - manapooli_sanakirja.get(valu, 0)):
+                return False
+            else:
+                manapooli_sanakirja["neutraali"] -= (maara - manapooli_sanakirja.get(valu, 0))
+                manapooli_sanakirja[valu] = max(0, manapooli_sanakirja[valu] - maara)
 
     return True
 
 def muunna_mana_sanakirjaksi(mana):
-    sanakirja = {}
+    sanakirja = {"neutraali": 0}
 
-    osat = mana.split()
-    for i in range(0, len(osat), 2):
-        valu = muunna_valu_suomenkieliseksi(osat[i])
-        maara = int(osat[i+1])
-        sanakirja[valu] = maara
+    while mana:
+        if mana[0].isdigit():
+            maara = ""
+            while mana and mana[0].isdigit():
+                maara += mana[0]
+                mana = mana[1:]
+            maara = int(maara)
+        else:
+            maara = 1
+
+        if not mana:
+            break
+
+        if mana[0] == "r":
+            valu = "punainen"
+        elif mana[0] == "g":
+            valu = "vihreä"
+        elif mana[0] == "u":
+            valu = "sininen"
+        elif mana[0] == "b":
+            valu = "musta"
+        elif mana[0] == "w":
+            valu = "valkoinen"
+        else:
+            valu = "neutraali"
+
+        mana = mana[1:]
+
+        if valu in sanakirja:
+            sanakirja[valu] += maara
+        else:
+            sanakirja[valu] = maara
 
     return sanakirja
 
-def muunna_valu_suomenkieliseksi(valu):
-    if valu == "r":
-        return "punainen"
-    elif valu == "g":
-        return "vihreä"
-    elif valu == "u":
-        return "sininen"
-    elif valu == "b":
-        return "musta"
-    elif valu == "w":
-        return "valkoinen"
-    else:
-        return "neutraali"
 
-manapooli_str = input("Syötä manapoolin valu- ja määrätiedot välilyönnillä erotettuna (esim. neutraali 3 punainen 2): ")
-kortin_mana_str = input("Syötä kortin manan valu- ja määrätiedot välilyönnillä erotettuna (esim. neutraali 2 punainen 2): ")
+
+manapooli_str = input("Syötä manapoolin valu- ja määrätiedot välilyönnillä erotettuna (esim. 2 r r): ")
+kortin_mana_str = input("Syötä kortin manan valu- ja määrätiedot välilyönnillä erotettuna (esim. 2 r r): ")
 
 if tarkista_mana(manapooli_str, kortin_mana_str):
     print("Sinulla on tarpeeksi manaa!")
